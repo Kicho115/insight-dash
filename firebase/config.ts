9389da614c50ed39aebb13e69aab17e79c842c46
Kicho115 <oscar.leonardo.ang@gmail.com>
2025-08-25 19:34:07 -0700

    Firebase auth and firestore setup

---
+++ firebase/config.ts
@@ -0,0 +1,22 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCknmcjiKWtVkLlQ6t_JkuWu8fXEgnbM50",
+  authDomain: "insight-dash.firebaseapp.com",
+  projectId: "insight-dash",
+  storageBucket: "insight-dash.firebasestorage.app",
+  messagingSenderId: "811846802264",
+  appId: "1:811846802264:web:19e31fd15c029df349a32d",
+  measurementId: "G-RFHQSR0CK8",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+// Export firebase services instances
+export const auth = getAuth(app);
+export const db = getFirestore(app);
