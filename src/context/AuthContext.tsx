2482611b2f3c82d54c2b4ac91a3601c3868e9289
Kicho115 <oscar.leonardo.ang@gmail.com>
2025-08-27 16:09:00 -0700

    Add an Authentication Context for firebase

---
+++ src/context/AuthContext.tsx
@@ -0,0 +1,54 @@
+"use client";
+
+import {
+  useState,
+  useEffect,
+  createContext,
+  useContext,
+  ReactNode,
+} from "react";
+import { onAuthStateChanged, User } from "firebase/auth";
+import { auth } from "@/../firebase/config";
+
+interface AuthContextType {
+  user: User | null;
+  loading: boolean;
+}
+
+export const AuthContext = createContext<AuthContextType>({
+  user: null,
+  loading: true,
+});
+
+/**
+ * Hook to access the authentication context.
+ * Provides the current user and session loading state.
+ * @returns An object containing:
+ * - `user`: The Firebase user object if the session is active, or `null`.
+ * - `loading`: A boolean that is `true` while verifying the initial session.
+ */
+export const useAuthContext = () => useContext(AuthContext);
+
+export const AuthContextProvider = ({ children }: { children: ReactNode }) => {
+  const [user, setUser] = useState<User | null>(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setUser(user);
+      } else {
+        setUser(null);
+      }
+      setLoading(false);
+    });
+
+    return () => unsubscribe();
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ user, loading }}>
+      {loading ? <div>Loading...</div> : children}
+    </AuthContext.Provider>
+  );
+};
