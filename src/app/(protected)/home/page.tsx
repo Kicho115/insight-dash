a1d4b21c3027c4feee1f23788398a3b8d01622c9
Kicho115 <oscar.leonardo.ang@gmail.com>
2025-09-01 18:49:33 -0700

    Add basic greeting to the user in homepage

--- src/app/(protected)/home/page.tsx
+++ src/app/(protected)/home/page.tsx
@@ -1,8 +1,31 @@
+"use client";
+
+// React imports
+import { useState, useEffect } from "react";
+
 // Import CSS
 import styles from "./styles.module.css";
 
 const HomePage = () => {
-  return <div className={styles.container}></div>;
+  const [greeting, setGreeting] = useState("");
+
+  useEffect(() => {
+    const getGreeting = () => {
+      const hour = new Date().getHours();
+
+      if (hour >= 5 && hour < 12) {
+        return "Good Morning";
+      } else if (hour >= 12 && hour < 19) {
+        return "Good Afternoon";
+      } else {
+        return "Good Night";
+      }
+    };
+
+    setGreeting(getGreeting());
+  }, []);
+
+  return <div className={styles.container}>{greeting}</div>;
 };
 
 export default HomePage;
--- src/app/(protected)/layout.tsx
+++ src/app/(protected)/layout.tsx
@@ -1,12 +1,33 @@
+"use client";
+
+// Next.js imports
+import { useRouter } from "next/navigation";
+
+// React imports
+import { useEffect } from "react";
+
+// Import components
 import { Sidebar } from "@/components/sidebar";
+
+// Import authentication context
+import { useAuthContext } from "@/context/AuthContext";
 
 export default function ProtectedLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const { user } = useAuthContext();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (user) {
+      router.push("/");
+    }
+  }, [user, router]);
+
   return (
-    <div>
+    <div style={{ display: "flex", height: "100dvh" }}>
       <Sidebar />
       <main>{children}</main>
     </div>
